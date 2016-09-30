b09e9ead9cc1aa1e97fc70441409f9906cdcf66d
Feng Ye <1584214261@qq.com>
2016-09-30 14:04:21 +0800

    add back the name

---
+++ todo_app/__openerp__.py
@@ -0,0 +1,12 @@
+{
+	"name":"To-do Application",
+	"description":"Manager your task",
+	"author":"Feng YE",
+	"depends":['mail'],
+	"application":True,
+	"data":[
+		'todo_view.xml',
+		'security/ir.model.access.csv',
+		'security/todo_access_rules.xml'
+	]
+}
---
+++ todo_app/todo_model.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from openerp import models, fields, api
+class TodoTask(models.Model):
+	"""docstring for TodoTask"""
+	_name = 'todo.task'
+	name = fields.Char('Description', required = True)
+	is_done = fields.Boolean('Done?')
+	active = fields.Boolean('Active', required = True)
+
+	@api.one 
+	def do_toggle_done(self):
+		self.is_done = not self.is_done
+		return True #XMLRPC Methods from client requires return sth
+	@api.multi
+	def do_clear_done(self):
+		done_recs = self.search([('is_done','=',True)])
+		done_recs.write({'active':False})
+		return True
