d71364c5a85b55cec7bfe3791a4e5d2a55f2895b
Feng Ye <1584214261@qq.com>
2016-10-01 06:16:07 +0800

    Extended form view

--- todo_user/__openerp__.py
+++ todo_user/__openerp__.py
@@ -4,9 +4,7 @@
 	"author":"Feng YE",
 	"depends":['mail'],
 	# "application":True,
-	# "data":[
-	# 	'todo_view.xml',
-	# 	'security/ir.model.access.csv',
-	# 	'security/todo_access_rules.xml'
-	# ]
+	"data":[
+		'todo_view.xml',
+	]
 }
