73822a56a81de50f5b0a8e9a26a77b4ea116d186
Feng Ye <1584214261@qq.com>
2016-10-05 18:03:12 +0800

    add computed field and related field

--- todo_ui/todo_model.py
+++ todo_ui/todo_model.py
@@ -48,9 +48,22 @@ class TodoTask(models.Model):
 	tag_ids = fields.Many2many('todo.task.tag',string='Tags')
 	stage_fold = fields.Boolean(
 		'Stage Folded?',
-		compute='_compute_stage_fold')
+		compute='_compute_stage_fold',
+		# store = False) # the default
+		search = '_search_stage_fold',
+		inverse='_write_stage_fold'
+		)
+	stage_stage = fields.Selection(
+		related = 'stage_id.state',
+		string = 'Stage State')
 
 	@api.one
 	@api.depends('stage_id.fold')
 	def _compute_stage_fold(self):
 		self.stage_fold = self.stage_id.fold
+
+	def _search_stage_fold(self, operator, value):
+		return [('stage_id.fold',operator,value)]
+
+	def _write_stage_fold(self):
+		self.stage_id.fold = self.stage_fold
