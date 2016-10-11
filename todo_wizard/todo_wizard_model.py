3a83fdd792d61d9806354b5ffbd63d83c5a38428
Feng Ye <1584214261@qq.com>
2016-10-11 19:44:33 +0800

    try logging format

--- todo_wizard/todo_wizard_model.py
+++ todo_wizard/todo_wizard_model.py
@@ -53,10 +53,10 @@ class TodoWizard(models.TransientModel):
 	def do_populate_tasks(self):
 		self.ensure_one()
 		Task = self.env['todo.task']
-		logging.debug('Task:',Task)
+		logging.debug('Task:%s'%Task)
 		all_tasks = Task.search([])
-		logging.debug('all_tasks:',all_tasks)
+		logging.debug('all_tasks:%s'%all_tasks)
 		self.task_ids = all_tasks
-		logging.debug('self.task_ids:',self.task_ids)
+		logging.debug('self.task_ids:%s'%self.task_ids)
 		#  reopen wizard form on same wizard record
 		return self.do_reopen_form
