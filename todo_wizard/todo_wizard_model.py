76bc6d642ac079bc6f09a286a0c86ffc074691d0
Feng Ye <1584214261@qq.com>
2016-10-11 08:46:43 +0800

    add control logic to button: Count & Mass Update

--- todo_wizard/todo_wizard_model.py
+++ todo_wizard/todo_wizard_model.py
@@ -16,3 +16,23 @@ class TodoWizard(models.TransientModel):
 	_logger.info('An INFO message')
 	_logger.warning('A WARNING message')
 	_logger.error('An ERROR message')
+
+	@api.multi
+	def do_mass_update(self):
+		self.ensure_one()
+		if not (self.new_deadline or self.new_user_id):
+			raise exceptions.ValidationError('No data to update')
+		# else:
+		_logger.debug('Mass update on Todo Tasks %s',
+			self.task_ids.ids)
+		if self.new_deadline:
+			self.task_ids.write({'date_deadline':self.new_user_id})
+		if self.new_user_id:
+			self.task_ids.write({'user_id':self.new_user_id.id})
+
+	@api.multi
+	def do_count_tasks(self):
+		Task = self.env['todo.task']
+		count = Task.search_count([])
+		raise exceptions.Warning(
+			'There are %d active tasks.' %count)
